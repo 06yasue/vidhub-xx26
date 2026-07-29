4d31be98ec570b82d0044ca395b4dc5fda9160f2
06yasue <101648951+06yasue@users.noreply.github.com>
2026-07-29 21:59:08 +0700

    Create layout.tsx

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,24 @@
+export const metadata = { title: 'Video Portal' };
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="id">
+      <head>
+        {/* Bootstrap 3 CDN */}
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+      </head>
+      <body>
+        <nav className="navbar navbar-inverse">
+          <div className="container">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="/">My Video Portal</a>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
