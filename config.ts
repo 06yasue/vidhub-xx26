3f283b4b06290a5f6aee072f7ccbe470778f020e
06yasue <101648951+06yasue@users.noreply.github.com>
2026-07-30 15:26:36 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   site_name: "BFF62", 
   domain: "vidhub-xx26.vercel.app",  
-  url_ref: "https://link-offer-kamu.com/ref?id=123", 
+  url_ref: "https://moneyplatform.biz/partner/join/71005448.html", 
 };
